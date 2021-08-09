8344c5bf6f0170ef9edcd81bdf7755d912a48413
joesephorekhov <josephorekhov@gmail.com>
2021-08-09 16:07:00 -0600

    commit tree fb6ca363cb3af5f23d216c3a5b7973d51278e662

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
+  let msg;
+  if (distance <= 400){
+    msg = 'This one is on me!';
+  }
+
   // Write your code here!
 }
 
